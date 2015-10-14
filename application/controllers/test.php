6acd9773b57f22b2b8e6e31ed2ab4f237dccdd66
David <David@lpt2-zcabc48>
2015-10-14 16:41:57 +0100

    Started the rewrite of DecimillClient to support connection with the new decimill-server.

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -9,91 +9,10 @@ class Test extends CL_Controller {
         parent::__construct();
     }
     
-    public function file() {
-        echo var_dump(file_exists('C:/Users/David/Dev/dmtools/web/application/images/models/7/full.png'));
-    }
-    
-    public function bool() {
-        $val = 0.001;
-        if ($val) {
-            echo "yes";
-        } else {
-            echo "no";
-        }
-    }
-    
-    public function parsedown() {
-        $this->load->library('Parsedown');
-        $parser = new Parsedown();
-        echo $parser->text("**Hello**\n\nThis is an inline `Code` here.\n    Minimise CO2 Emissions\n    co2_emissions\nEnd _ending_.[Link](http://www.google.com).");
-    }
-    
-    public function markdown_js() {
-        $this->load->view('markdown_js');
-    }
-    
-    public function gen() {
-//        require APPPATH . 'libraries/MarkdownInterface.php';
-//        require APPPATH . 'libraries/MarkdownExtra.php';
-//        require APPPATH . 'libraries/Markdown.php';
-        $this->load->library('MarkdownInterface');
-        $this->load->library('Markdown');
-        $this->load->library('MarkdownExtra');
-        
-        echo Markdown::defaultTransform("*Test*");
-    }
-
-    public function user() {
-
-        $this->load->model('UserModel');
-        
-//        $user = new UserModel();
-//        $user->setEmail('ania.obolewicz@gmail.com');
-//        $user->setPassword(md5('AniaIsCool'));
-//        
-//        if ($user->save() === FALSE) {
-//            echo "User with email " . $user->getEmail() . " already exists!";
-//            return;
-//        }
-//        
-//        echo $user->getId();
-        
-        $user = UserModel::loadById(3);
-        
-        $user->setEmail('ania.obolewicz@gmail.com');
-        $user->setPassword(md5('AniaIsCool'));
-        $user->save();
-        
-        $user = UserModel::loadById(1);
-        
-        if ($user !== NULL) {
-            echo $user->getId();
-            echo $user->getEmail();
-            echo $user->getPassword() == md5('K8rnatt0');
-        }
-    }
-
-    function case_study() {
-        
-        $this->load->model('CaseModel');
-        print_r(CaseModel::loadByUser(1));
-    }
-    
-    function models() {
-        
-        $this->load->model('ModelModel');
-        $models = ModelModel::loadByCase(1);
-        print_r($models);
-    }
-    
-    function client() {
-        
+    public function client() {
         $this->load->library('DecimillClient');
-        DecimillClient::call("-a", "parseModel", "-m", "1");
-    }
-    
-    function chart() {
-        $this->load->view('backup/chart');
+        $client = new DecimillClient();
+        $client->compileModel(2, 'UCL', 'This model `Goal 1 <- Goal 3, Goal 5` is nice.');
     }
         
 }
--- application/libraries/DecimillClient.php
+++ application/libraries/DecimillClient.php
@@ -2,23 +2,68 @@
 
 class DecimillClient {
 
+    private $curl = NULL;
+
+    public function __construct() {
+        $this->curl = curl_init();
+    }
+
+    public function compileModel($id, $namespace, $text) {
+
+        $request = [
+            'action' => 'model',
+            'model' => [
+                'id' => $id,
+                'namespace' => $namespace,
+                'text' => $text
+            ]
+        ];
+        
+        $requestBody = json_encode($request);
+
+        curl_setopt($this->curl, CURLOPT_URL, 'http://localhost/compile');
+        curl_setopt($this->curl, CURLOPT_PORT, 88);
+        curl_setopt($this->curl, CURLOPT_POSTFIELDS, 88);
+        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($this->curl, CURLOPT_POST, 1);
+        curl_setopt($this->curl, CURLOPT_POSTFIELDS, $requestBody);
+        curl_setopt($this->curl, CURLOPT_HTTPHEADER, [
+            'Content-Type: text/plain',
+            'Content-Length: ' . strlen($requestBody)
+        ]);
+        
+        $rawResponse = curl_exec($this->curl);
+        $response = json_decode($rawResponse, TRUE);
+        
+        curl_close($this->curl);
+        
+        if ($response['status'] == 'OK') {
+            echo $response['body'] . '<br />';
+            foreach ($response['paths'] AS $path) {
+                echo '<img src="http://localhost:88' . $path . '" />';
+            }
+        } else {
+            echo "Model must be broken!";
+        }
+    }
+
     public static function call() {
-        
+
         $args = implode(" ", func_get_args());
-        
+
         $config = get_config();
         $clientPath = $config['decimill_client_path'];
         $configPath = $config['decimill_client_config'];
-        
+
 //        echo "java -jar $clientPath $configPath $args";
 //        exit();
-        
+
         exec("java -jar $clientPath $configPath $args", $out, $ret);
-        
+
 //        var_dump($ret);
 //        print_r($out);
 //        exit();
-        
+
         if (count($out) > 0) {
             return json_decode($out[0]);
         }
